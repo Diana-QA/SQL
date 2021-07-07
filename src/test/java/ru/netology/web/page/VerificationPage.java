4fffd9ebe957e077d19ea4aa7c969598370a3a1a
Diana Krapivina <mr.ikoma@mail.ru>
2021-07-07 20:19:35 +0300

    Add Page Objects

--- src/test/java/ru/netology/web/page/VerificationPage.java
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -1,4 +1,22 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+
 public class VerificationPage {
+    private final SelenideElement codeField = $("[data-test-id=code] input");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
+
+    public VerificationPage() {
+        codeField.shouldBe(Condition.visible);
+    }
+
+    public DashboardPage validVerify(String code) {
+        codeField.setValue(code);
+        verifyButton.click();
+        return new DashboardPage();
+    }
+
 }
